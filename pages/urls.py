5b9ad332fb46c484ae88406ece844897d1a40bfa
sammwaura <meshsami27@gmail.com>
2018-09-13 20:18:29 +0300

    add templates folder

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-
+from . import views
 
 urlpatterns = [
-
+	url('^$', views.home, name='home'),
+	url('^about/$', views.about, name='about'),
+	url('^contact/$', views.contact, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+	return render(request, "home.html", {})
+
+
+def home(request):
+	return render(request, "about.html", {})
+
+
+def home(request):
+	return render(request, "contact.html", {})
+
